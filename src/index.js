ef4bf330b6f3eb2acbe81975cda51b8dcfea6994
Noor ul Hasanat <noorulhasanat123@gmail.com>
2025-02-07 15:04:44 +0500

    changes in App.js index.js and indexCss

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store,persistor } from './Redux/Store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+    <StrictMode>
+        <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+        </Provider>
+    </StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
