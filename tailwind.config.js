0fa2e8c22b8f61b2da2fbbc6e4192c070e6e1cd9
Noor ul Hasanat <noorulhasanat123@gmail.com>
2025-02-07 15:32:19 +0500

    material icons and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      width:{
+        'custom-width': 'calc(100% - 16rem);',
+        'perc-width':'80%'
+      },
+      screens:{
+        'xs' : '900px',
+        'xxs' : '420px'
+      }
+    },
+  },
+  plugins: [],
+}
